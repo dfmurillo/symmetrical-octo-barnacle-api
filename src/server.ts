a1aa89c75ef151da3dd3813a3e4ca78b8a15fbf9
Daniel Florez <daniel.florez.murillo@gmail.com>
2021-02-18 23:58:48 +0100

    Added basic routes for quizzes (#3)

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import errorHandler from "errorhandler";
+import * as http from "http";
 import app from "./app";
 
+const server: http.Server = http.createServer(app);
 
 /**
  * Error Handler. Provides full stack
@@ -13,7 +15,7 @@ if (process.env.NODE_ENV === "development") {
 /**
  * Start Express server.
  */
-const server = app.listen(app.get("port"), () => {
+server.listen(app.get("port"), () => {
     console.log(
         "  App is running at http://localhost:%d in %s mode",
         app.get("port"),
