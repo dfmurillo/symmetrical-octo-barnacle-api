906f6ee7438401eb6471d68e4d830dcc522af706
Daniel Flórez Murillo <daniel.florez.murillo@gmail.com>
2021-02-21 16:25:30 +0100

    Tests added

--- src/quizzes/quizzes.controller.test.ts
+++ src/quizzes/quizzes.controller.test.ts
@@ -17,55 +17,111 @@ describe("Tests for quizzes middleware", () => {
       json: jest.fn(),
     };
   });
-  it("Given we want to get all quizzes, when calling listQuizzes controller, then success is answered", () => {
+  
+  describe("listQuizzes", () => {
+    it("Given we want to get all quizzes, when calling listQuizzes controller, then success is answered", () => {
+      const quizzes: IQuizzes = [{
+        id: 11,
+        name: "Quiz 1"
+      }, {
+        id: 22,
+        name: "Quiz 2"
+      }, {
+        id: 33,
+        name: "Quiz 3"
+      }];
+  
+      MockedQuizzesService.getAllQuizzes.mockReturnValue(quizzes);
+  
+      const expectedResponse = {
+        quizzes
+      };
+      QuizzesController.listQuizzes(mockRequest as Request, mockResponse as Response);
+      expect(mockResponse.json).toBeCalledWith(expectedResponse);
+    });
 
-    const quizzes: IQuizzes = [{
-      id: 11,
-      name: "Quiz 1"
-    }, {
-      id: 22,
-      name: "Quiz 2"
-    }, {
-      id: 33,
-      name: "Quiz 3"
-    }];
+    it("Given there are no quizzes to show, when calling listQuizzes controller, then error is answered", () => {
+      MockedQuizzesService.getAllQuizzes.mockImplementation(() => {
+        throw new Error("Oops I did it again!");
+      });
 
-    MockedQuizzesService.getAllQuizzes.mockReturnValue(quizzes);
-
-    const expectedResponse = {
-      quizzes
-    };
-    QuizzesController.listQuizzes(mockRequest as Request, mockResponse as Response);
-    expect(mockResponse.json).toBeCalledWith(expectedResponse);
+      QuizzesController.listQuizzes(mockRequest as Request, mockResponse as Response);
+      expect(mockResponse.json).toBeCalledWith({error: "Oops I did it again!"});
+    });
   });
 
-  it("Given we want to get a quiz by id, when calling getQuizById controller, then success is answered", () => {
-    const quizIdToGet = "44";
-    const quiz: IQuiz = {
-      name: "My fancy quizz",
-      questions: [{
-        questionId: 11,
-        question: "My fancy question 1",
-        answers: [{
-          answerId: 12,
-          answer: "Fancy Answer 1"
+  describe("getQuizById", () => {
+    it("Given we want to get a quiz by id, when calling getQuizById controller, then success is answered", () => {
+      const quizIdToGet = "44";
+      const quiz: IQuiz = {
+        name: "My fancy quizz",
+        questions: [{
+          questionId: 11,
+          question: "My fancy question 1",
+          answers: [{
+            answerId: 12,
+            answer: "Fancy Answer 1"
+          }]
         }]
-      }]
-    };
+      };
+  
+      MockedQuizzesService.getQuizById.mockReturnValue(quiz);
+      mockRequest = {
+        params: {
+          quizId: quizIdToGet
+        }
+      };
+      const expectedResponse = {
+        quiz
+      };
+  
+      QuizzesController.getQuizById(mockRequest as Request, mockResponse as Response);
+      expect(mockResponse.json).toBeCalledWith(expectedResponse);
+    });
+ 
+      it("Given we want to get a quiz that dont exist, when calling getQuizById controller, then error is answered", () => {
+        MockedQuizzesService.getQuizById.mockImplementation(() => {
+          throw new Error("Oops I did it again!");
+        });
+  
+        mockRequest = {
+          params: {
+            quizId: "99"
+          }
+        };
 
-    MockedQuizzesService.getQuizById.mockReturnValue(quiz);
-    mockRequest = {
-      params: {
-        quizId: quizIdToGet
-      }
-    };
-    const expectedResponse = {
-      quiz
-    };
-
-    QuizzesController.getQuizById(mockRequest as Request, mockResponse as Response);
-    expect(mockResponse.json).toBeCalledWith(expectedResponse);
+        QuizzesController.getQuizById(mockRequest as Request, mockResponse as Response);
+        expect(mockResponse.json).toBeCalledWith({error: "Oops I did it again!"});
+      });
   });
 
+  describe("validateQuiz", () => {
+    it("Given we want to validate an existent quiz, when we call validateQuiz, then the value will be returned", () => {
+      MockedQuizzesService.validateQuizAnswers.mockReturnValue(80);
+  
+      mockRequest = {
+        body: {
+          quizId: "11"
+        }
+      };
+
+      QuizzesController.validateQuiz(mockRequest as Request, mockResponse as Response);
+      expect(mockResponse.json).toBeCalledWith({quizResult: 80});
+    });
+
+    it("Given we validate a quiz that dont exist, when calling validateQuiz, then we return the error", () => {
+      MockedQuizzesService.validateQuizAnswers.mockImplementation(() => {
+        throw new Error("Oops I did it again!");
+      });
+
+      mockRequest = {
+        body: {
+          quizId: "11"
+        }
+      };
+      QuizzesController.validateQuiz(mockRequest as Request, mockResponse as Response);
+      expect(mockResponse.json).toBeCalledWith({error: "Oops I did it again!"});
+    });
+  });
 
 });
--- src/quizzes/quizzes.controller.ts
+++ src/quizzes/quizzes.controller.ts
@@ -20,7 +20,7 @@ class QuizzesController {
         response.json({ quizzes });
       } catch(error) {
         response.statusCode = 404;
-        response.json({ error });
+        response.json({error: error.message});
       }
     }
 
@@ -31,19 +31,19 @@ class QuizzesController {
         response.json({ quiz });
       } catch(error) {
         response.statusCode = 404;
-        response.json({ error });
+        response.json({ error: error.message });
       }
     }
 
     validateQuiz(request: ValidatedRequest<IAnsweredSchema>, response: Response) {
-      const { quizId, questions } = request.body;
       try {
+        const { quizId, questions } = request.body;
         const quizResult = QuizzesService.validateQuizAnswers(quizId, questions);
         response.statusCode = 200;
         response.json({ quizResult });
       } catch (error) {
         response.statusCode = 500;
-        response.json({ error });
+        response.json({ error: error.message });
       }
     }
 }
